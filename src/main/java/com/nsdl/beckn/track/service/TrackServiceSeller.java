2f399bb79419c2821406265fcdce47bdd4785a7f
Somisetti Venkata Sai Sudheep Rao <sai.sudheep@BLRETV-C02GL011.local>
2022-04-04 11:12:15 +0530

    intial changes for track api

--- src/main/java/com/nsdl/beckn/track/service/TrackServiceSeller.java
+++ src/main/java/com/nsdl/beckn/track/service/TrackServiceSeller.java
@@ -8,20 +8,27 @@ import org.slf4j.LoggerFactory;
 import com.nsdl.beckn.common.model.ConfigModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nsdl.beckn.api.model.common.Context;
+import com.nsdl.beckn.api.model.ontrack.OnTrackMessage;
+import com.nsdl.beckn.api.model.ontrack.OnTrackRequest;
+
 import java.util.concurrent.CompletableFuture;
 import com.nsdl.beckn.api.model.common.Ack;
 import com.nsdl.beckn.api.enums.AckStatus;
 import com.nsdl.beckn.api.model.response.ResponseMessage;
 import com.nsdl.beckn.api.model.response.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.nsdl.beckn.track.extension.Schema;
+
 import org.springframework.http.HttpHeaders;
 import com.nsdl.beckn.common.util.JsonUtil;
 import com.nsdl.beckn.common.validator.BodyValidator;
 import com.nsdl.beckn.common.service.ApplicationConfigService;
 import com.nsdl.beckn.common.sender.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
@@ -40,6 +47,10 @@ public class TrackServiceSeller
     private BodyValidator bodyValidator;
     @Autowired
     private JsonUtil jsonUtil;
+
+    @Autowired
+    @Value("classpath:dummyResponses/onTrack.json")
+    private Resource resource;
     
     public ResponseEntity<String> track(final HttpHeaders httpHeaders, final Schema request) throws JsonProcessingException {
         TrackServiceSeller.log.info("Going to validate json request before sending to buyer...");
@@ -53,19 +64,50 @@ public class TrackServiceSeller
         adaptorResponse.setMessage(resMsg);
         final Context ctx = request.getContext();
         adaptorResponse.setContext(ctx);
+
         //CompletableFuture.runAsync(this::lambda$track$0);
+        CompletableFuture.runAsync(() -> {
+            this.sendRequestToSellerInternalApi(httpHeaders, request);
+        });
+
         return (ResponseEntity<String>)new ResponseEntity((Object)this.mapper.writeValueAsString((Object)adaptorResponse), HttpStatus.OK);
     }
     
     private void sendRequestToSellerInternalApi(final HttpHeaders httpHeaders, final Schema request) {
         TrackServiceSeller.log.info("sending request to seller internal api [in seperate thread]");
         try {
-            final Context context = request.getContext();
-            final String bppId = context.getBppId();
-            final ConfigModel configModel = this.configService.loadApplicationConfiguration(bppId, "track");
+            final ConfigModel configModel = this.configService.loadApplicationConfiguration(request.getContext().getBppId(), "track");
             final String url = configModel.getMatchedApi().getHttpEntityEndpoint();
             final String json = this.jsonUtil.toJson((Object)request);
-            this.sendRequest.send(url, httpHeaders, json, configModel.getMatchedApi());
+            //     this.sendRequest.send(url, httpHeaders, json, configModel.getMatchedApi());
+
+            if(!"true".equals(configModel.getDisableAdaptorCalls())){
+                String resp = this.sendRequest.send(url, httpHeaders, json, configModel.getMatchedApi());
+                TrackServiceSeller.log.info("Response from ekart adaptor: " + resp);
+            }
+
+            //creating a dummy response
+            OnTrackMessage onTrack = this.mapper.readValue(this.resource.getInputStream(), OnTrackMessage.class);
+            TrackServiceSeller.log.info(onTrack.toString());
+
+            OnTrackRequest respBody = new OnTrackRequest();
+            respBody.setContext(request.getContext());
+            respBody.getContext().setAction("on_track");
+            respBody.getContext().setBppId(configModel.getSubscriberId());
+            respBody.getContext().setBppUri(configModel.getSubscriberUrl());
+            httpHeaders.remove("host");
+
+            respBody.setMessage(onTrack);
+            String respJson = this.jsonUtil.toJson((Object)respBody);
+
+            String host = httpHeaders.get("remoteHost").get(0);
+            if("0:0:0:0:0:0:0:1".equals(host)) {
+                host="localhost";
+            }
+
+            String onTrackresp = this.sendRequest.send(respBody.getContext().getBapUri() +"on_track",
+                    httpHeaders, respJson, configModel.getMatchedApi());
+            TrackServiceSeller.log.info(onTrackresp);
         }
         catch (Exception e) {
             TrackServiceSeller.log.error("error while sending post request to seller internal api" + e);
